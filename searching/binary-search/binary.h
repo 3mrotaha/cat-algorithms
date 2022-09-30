d09a38fdbc32c9b036d8ed912877efa4c1b2f288
Amr M. Taha <83893565+3mrotaha@users.noreply.github.com>
2022-09-30 21:55:10 +0200

    Delete searching/binary-search directory

--- searching/binary-search/binary.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BS_
-#define _BS_
-
-int binarySearch(int[], int, int);
-
-int binarySearchRec(int[], int, int);
-
-#endif
--- searching/binary-search/helpers.h
+++
@@ -1,6 +0,0 @@
-#ifndef _BS_HELPERS_
-#define _BS_HELPERS_
-
-static int BSR(int[], int, int, int);
-
-#endif
